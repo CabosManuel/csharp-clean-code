a97a46505034c747544554a1e831591278f56de6
cabosmanuel <mcabos.dev@gmail.com>
2026-05-13 22:10:04 -0500

    ref: Aplicando mejoras .NET 6 y 7
    - En C# 10 .NET 6 ahora se usa namespace separado fuera de llaves, como PHP
    - C# 11 .NET 7 ahora se usa triple doble commila para textos con salgto de linea sin \

--- Program.cs
+++ Program.cs
@@ -1,117 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ToDo
+namespace ToDo;
+
+internal class Program
 {
-    internal class Program
+    public static List<string> TaskList { get; set; } = new List<string>();
+
+    static void Main(string[] args)
     {
-        public static List<string> TaskList { get; set; } = new List<string>();
-
-        static void Main(string[] args)
+        Menu menuSelected;
+        do
         {
-            Menu menuSelected;
-            do
+            menuSelected = (Menu) ShowMainMenu();
+            switch (menuSelected)
             {
-                menuSelected = (Menu) ShowMainMenu();
-                switch (menuSelected)
-                {
-                    case Menu.Add: ShowMenuAdd();
-                        break;
-                    case Menu.Remove: ShowMenuRemove();
-                        break;
-                    case Menu.List: ShowMenuTaskList();
-                        break;
-                }
-            } while (menuSelected != Menu.Exit);
-        }
-        /// <summary>
-        /// Show the main menu 
-        /// </summary>
-        /// <returns>Returns option indicated by user</returns>
-        public static int ShowMainMenu()
-        {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("Ingrese la opción a realizar: ");
-            Console.WriteLine("1. Nueva tarea");
-            Console.WriteLine("2. Remover tarea");
-            Console.WriteLine("3. Tareas pendientes");
-            Console.WriteLine("4. Salir");
+                case Menu.Add: ShowMenuAdd();
+                    break;
+                case Menu.Remove: ShowMenuRemove();
+                    break;
+                case Menu.List: ShowMenuTaskList();
+                    break;
+            }
+        } while (menuSelected != Menu.Exit);
+    }
+    /// <summary>
+    /// Show the main menu 
+    /// </summary>
+    /// <returns>Returns option indicated by user</returns>
+    public static int ShowMainMenu()
+    {
+        Console.WriteLine(""" 
+            Ingrese la opción a realizar:
+            1. Nueva tarea
+            2. Remover tarea
+            3. Tareas pendientes
+            4. Salir
+            """
+        );
 
-            // Read line
+        // Read line
+        string line = Console.ReadLine();
+        return Convert.ToInt32(line);
+    }
+
+    public static void ShowMenuRemove()
+    {
+        try
+        {
+            Console.WriteLine("Ingrese el número de la tarea a remover: ");
+            PrintTaskList(TaskList);
+
             string line = Console.ReadLine();
-            return Convert.ToInt32(line);
-        }
+            // Remove one position
+            int indexToRemove = Convert.ToInt32(line) - 1;
 
-        public static void ShowMenuRemove()
-        {
-            try
-            {
-                Console.WriteLine("Ingrese el número de la tarea a remover: ");
-                PrintTaskList(TaskList);
-
-                string line = Console.ReadLine();
-                // Remove one position
-                int indexToRemove = Convert.ToInt32(line) - 1;
-
-                if (indexToRemove > TaskList.Count - 1 || indexToRemove < 0)
-                    Console.WriteLine("Número de tarea inválido.");
-                else
-                {
-                    if (indexToRemove > -1 && TaskList.Count > 0)
-                    {
-                        string task = TaskList[indexToRemove];
-                        TaskList.RemoveAt(indexToRemove);
-                        Console.WriteLine($"Tarea {task} eliminada");
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Error al eliminar la tarea.");
-            }
-        }
-
-        public static void ShowMenuAdd()
-        {
-            Console.WriteLine("Ingrese el nombre de la tarea: ");
-            string task = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(task))
-            {
-                Console.WriteLine("La tarea no puede estar vacía.");
-                return;
-            }
-
-            TaskList.Add(task);
-            Console.WriteLine("Tarea registrada");
-        }
-
-        public static void ShowMenuTaskList()
-        {
-            if (TaskList?.Count > 0)
-            {
-                PrintTaskList(TaskList);
-            } 
+            if (indexToRemove > TaskList.Count - 1 || indexToRemove < 0)
+                Console.WriteLine("Número de tarea inválido.");
             else
             {
-                Console.WriteLine("No hay tareas por realizar");
+                if (indexToRemove > -1 && TaskList.Count > 0)
+                {
+                    string task = TaskList[indexToRemove];
+                    TaskList.RemoveAt(indexToRemove);
+                    Console.WriteLine($"Tarea {task} eliminada");
+                }
             }
         }
-
-        private static void PrintTaskList(List<string> TaskList)
+        catch (Exception)
         {
-            Console.WriteLine("----------------------------------------");
-            // Using foreach
-            TaskList.ForEach( task => Console.WriteLine($"{TaskList.IndexOf(task) + 1}. {task}") );
-            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Error al eliminar la tarea.");
         }
     }
 
-    public enum Menu
+    public static void ShowMenuAdd()
     {
-        Add = 1,
-        Remove = 2,
-        List = 3,
-        Exit = 4,
+        Console.WriteLine("Ingrese el nombre de la tarea: ");
+        string task = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(task))
+        {
+            Console.WriteLine("La tarea no puede estar vacía.");
+            return;
+        }
+
+        TaskList.Add(task);
+        Console.WriteLine("Tarea registrada");
     }
+
+    public static void ShowMenuTaskList()
+    {
+        if (TaskList?.Count > 0)
+        {
+            PrintTaskList(TaskList);
+        } 
+        else
+        {
+            Console.WriteLine("No hay tareas por realizar");
+        }
+    }
+
+    private static void PrintTaskList(List<string> TaskList)
+    {
+        Console.WriteLine("----------------------------------------");
+        // Using foreach
+        TaskList.ForEach( task => Console.WriteLine($"{TaskList.IndexOf(task) + 1}. {task}") );
+        Console.WriteLine("----------------------------------------");
+    }
+}
+
+public enum Menu
+{
+    Add = 1,
+    Remove = 2,
+    List = 3,
+    Exit = 4,
 }
